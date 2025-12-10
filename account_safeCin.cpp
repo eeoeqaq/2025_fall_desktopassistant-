6b1be93af43ccc5de38d5af79ad9d2890c36cee4
boyang <3292262949@qq.com>
2025-12-10 09:01:03 +0800

    Add files via upload

---
+++ account_safeCin.cpp
@@ -0,0 +1,23 @@
+#include "account.h"
+
+int safeCin(int& input, int max, int min, int type)
+{
+	
+	while (type == 1)
+	{
+		if(cin>>input)
+		for (int i = min; i <= max; i++)
+		{
+			if (input == i) return input;
+		}
+		cout << "输入非法，请重新输入" << endl;
+	}
+	
+	while (type == 2)
+	{
+		
+		if ((cin >> input) && input >= min && input <= max)
+			return input;
+		else  cout << "输入非法，请重新输入" << endl;
+	}
+}
